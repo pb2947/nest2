1181c4ddb33f803af98efff17932df6c3c4f104b
Prakash Bhandari <pb2947@gmail.com>
2024-10-17 21:32:27 +0545

    Install @nestjs/typeorm, typeorm, mysql2 and Configure TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -13,6 +14,23 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/public/',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_CONNECTION'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + '/**/*.entity.{ts,js}'],
+          synchronize: configService.get('DB_SYNC') === 'true',
+          keepConnectionAlive: true,
+        } as TypeOrmModuleAsyncOptions;
+      },
     }),
   ],
   controllers: [AppController],
