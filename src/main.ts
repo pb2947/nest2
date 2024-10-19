314fec837507ca8a704a97d6310053e00ca682c9
Prakash Bhandari <pb2947@gmail.com>
2024-10-19 21:01:14 +0545

    Update swagger module with bearer auth

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,14 @@ async function bootstrap() {
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('api')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('openapi', app, document);
+  SwaggerModule.setup('openapi', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   await app.listen(15000, function () {
     console.log('App is ready at http://localhost:15000');
